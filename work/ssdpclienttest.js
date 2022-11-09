3b6502dad5ce1eac0d15d7622be18723fca9b7ed
David Creager <david.h.creager@btinternet.com>
2022-11-09 18:13:25 +0000

    improve ssdpclienttest

--- work/ssdpclienttest.js
+++ work/ssdpclienttest.js
@@ -1,27 +1,31 @@
 var ssdp = require('node-ssdp').Client
   , client = new ssdp({})
-
+const searchQuery = ( (process.argv.length>2) ? process.argv[2] : "ssdp:all")
+const fullOrSummary = ( (process.argv.length>3) ? process.argv[3] : "summary")
+console.log("args should be ssdpclienttest <search string><full|summary>")
+console.log("searching for " + searchQuery + " showing " + fullOrSummary)
 client.on('notify', function () {
   //console.log('Got a notification.')
 })
-let STS = {};
-let first = true;
-client.on('response', function inResponse(headers, code, rinfo) {
-//  console.log('Got a response to an m-search:\n%d\n%s\n%s', code, JSON.stringify(headers, null, '  '), JSON.stringify(rinfo, null, '  '))
-	//if (!STS[headers.USN]) {
-		let wk = headers.LOCATION;
-		if (headers.LOCATION.indexOf("description.xml")!=-1) wk = headers.LOCATION.slice(0,(headers.LOCATION.indexOf("description.xml")));
-		console.log("location =" + wk + " ST=" + headers.ST + " USN=" + headers.USN )
-		//console.log("location =" + headers.LOCATION + " ST=" + headers.ST + " USN=" + headers.USN )
-		STS[headers.USN] = headers.ST;
-		if (first) {
-			first = false;
-			console.log(JSON.stringify(headers));
-		} else {
-			//console.log("," + JSON.stringify(headers));
-		}
-	//}
 
+
+let foundDevices = {};
+client.on('response', function inResponse(headers, code, rinfo) {
+		let wk = headers.LOCATION;
+		const tmp = !(! (headers.ST == searchQuery || searchQuery == "ssdp:all"))
+		if (tmp) {
+			if (!headers.USN || !foundDevices[headers.USN]) {
+				if (fullOrSummary == "summary") {
+					if (headers.LOCATION.indexOf("description.xml")!=-1) wk = headers.LOCATION.slice(0,(headers.LOCATION.indexOf("description.xml")));
+					console.log("stchk=" + tmp + " location = " + wk + " ST=" + headers.ST + " USN=" + headers.USN )
+				} else {
+					console.log("--------------------------------------------------------------------------------------")
+					console.log("location =" + headers.LOCATION + " ST=" + headers.ST + " USN=" + headers.USN )
+					console.log(JSON.stringify(headers))
+				}
+				foundDevices[headers.USN] = headers;
+			}
+		}
 })
 //client.search('urn:smartthings-com:device:')
 //client.search('ssdp:all')
@@ -29,7 +33,8 @@ client.on('response', function inResponse(headers, code, rinfo) {
 //client.search("urn:smartthings-com:device:thingsim:1")
 //urn:schemas-upnp-org:device:smartdev:1
 //client.search("urn:schemas-upnp-org:device:smartdev:1")
-client.search("urn:schemas-upnp-org:device:smartdev")
+//client.search("urn:schemas-upnp-org:device:smartdev")
+client.search(searchQuery);
 // Or maybe if you want to scour for everything after 5 seconds
 setInterval(function() {
   //client.search('ssdp:all')
